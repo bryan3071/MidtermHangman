fc57ca8c39a1fe0213c9b7e9687f5e9f0a11508a
bryan <bryan_hallman29@yahoo.com>
2018-10-29 12:29:59 -0400

    add random word generator

--- src/MidtermHangman/MidtermHangman.java
+++ src/MidtermHangman/MidtermHangman.java
@@ -1,11 +1,29 @@
 package MidtermHangman;
-
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 public class MidtermHangman {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int Attempts = 0;
+		
 		System.out.println("Welcome to a friendly game of hangman");
+		
+		
+		// Creates array of 10 different strings hard coded
+		String[] Words = {"Encapsulation", "Polymorphism", "Class", "Inheritance", "Superclass", "Subclass",
+				"Abstraction", "Extends", "Method", "Object"};
+		
+		//Handles the generation of 
+		Random random = new Random();
+		int randomIdx = random.nextInt(Words.length);
+		System.out.println(Words[randomIdx]);
+		
+		// creates array that holds selected characters
+		//char[] lettersSelected = new char[Words[randomWordNumber].length()];
+		
 		
 	}
 
