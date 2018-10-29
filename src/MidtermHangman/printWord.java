8dd38055ac4ee68d6097dd938de4ba6ad0200fb3
Romel levy <Romelestefan@gmail.com>
2018-10-29 12:55:24 -0400

    new printword class

---
+++ src/MidtermHangman/printWord.java
@@ -0,0 +1,27 @@
+package MidtermHangman;
+
+public class printWord {
+
+	/*
+	 * Print word with underscore for hidden letters, returns true if underscores
+	 * were printed, otherwise return false
+	 */
+
+	public static boolean printWord(String word, char[] enteredLetters) {
+		boolean underscorePrinted = false;
+		for (int i = 0; i < word.length(); i++) {
+			char letter = word.charAt(i);
+			// check for already entered letters from user.
+			if (inEnteredLetters(letter, enteredLetters))
+				System.out.print(letter); // If yes - print it
+			else {
+				System.out.print('_');
+				underscorePrinted = true;
+			}
+
+			return underscorePrinted;
+
+		}
+
+	}
+}
